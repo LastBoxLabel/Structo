dd3acc3aa6056d1f077e1b05c3300b7875e98c88
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-11-26 13:41:48 -0300

    feat: add base project

---
+++ src/main/java/tech/lastbox/structo/StructoApplication.java
@@ -0,0 +1,13 @@
+package tech.lastbox.structo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/lastbox/structo/StructoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lastbox.structo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
