fbd8561da8acd8ad100fd0e5be448035706dda03
Matheus <math.lima.m@gmail.com>
2024-11-26 22:12:31 -0300

    feat: add ProjectRepository

---
+++ src/main/java/tech/lastbox/structo/repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package tech.lastbox.structo.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.lastbox.structo.model.ProjectEntity;
+
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {}
