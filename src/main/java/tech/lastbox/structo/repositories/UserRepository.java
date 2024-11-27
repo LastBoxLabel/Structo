5b3bb6c8e0966cbea7cbf9b4eb29377515f58495
Matheus <math.lima.m@gmail.com>
2024-11-27 09:58:28 -0300

    feat: add @UserHandler annotation from LastShield 🥳🥳🥳

--- src/main/java/tech/lastbox/structo/repositories/UserRepository.java
+++ src/main/java/tech/lastbox/structo/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package tech.lastbox.structo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tech.lastbox.lastshield.security.core.annotations.UserHandler;
 import tech.lastbox.structo.model.UserEntity;
 
+@UserHandler
 public interface UserRepository extends JpaRepository<UserEntity, Long> {}
