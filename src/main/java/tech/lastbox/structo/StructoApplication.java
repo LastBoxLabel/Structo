2039336fb6d54664f628e4079b52882fad62ff2f
Matheus <math.lima.m@gmail.com>
2024-11-27 09:59:24 -0300

    refact: StructoApplication

--- src/main/java/tech/lastbox/structo/StructoApplication.java
+++ src/main/java/tech/lastbox/structo/StructoApplication.java
@@ -3,6 +3,7 @@ package tech.lastbox.structo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StructoApplication {
 
