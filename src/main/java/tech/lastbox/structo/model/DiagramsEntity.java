6f32fb14540ee16a5ed32e12aa991c79a2532268
Matheus <math.lima.m@gmail.com>
2024-11-26 22:11:48 -0300

    feat: add DiagramsEntity

--- src/main/java/tech/lastbox/structo/model/DiagramsEntity.java
+++ src/main/java/tech/lastbox/structo/model/DiagramsEntity.java
@@ -3,7 +3,7 @@ package tech.lastbox.structo.model;
 import jakarta.persistence.*;
 
 @Embeddable
-public class DiagramsEmbeddable {
+public class DiagramsEntity {
 
     @Column(nullable = false)
     private String mindmap;
